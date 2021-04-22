1c037afb42429a539216f46892d3356e5dbf0fcf
saanyan <62740358+saanyan@users.noreply.github.com>
2021-04-22 22:11:57 +0800

    Update insert_function.sql

--- sql/insert_function.sql
+++ sql/insert_function.sql
@@ -1,13 +1,17 @@
-DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text,text,text,text,text);
+DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text,text,text,text,text,text,text,text,text);
 --Assumes only one value being inserted
 
 CREATE OR REPLACE FUNCTION insert_crowd_mapping_data (
     _geojson TEXT,
     _name TEXT,
-    _product TEXT,
-    _price TEXT,
-    _contact TEXT,	
-    _address TEXT, 
+    _supplies TEXT,
+    _contact TEXT,
+    _number TEXT,	
+    _street_address TEXT,
+    _barangay TEXT,
+    _municipality_city TEXT,
+    _province TEXT,	
+    _region TEXT, 	
     _sched TEXT,
     _more TEXT)    
 --Has to return something in order to be used in a "SELECT" statement
@@ -23,9 +27,9 @@ BEGIN
 	
 
 	--Executes the insert given the supplied geometry, description, and username, while protecting against SQL injection.
-    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, name, product, price, contact, address, sched, more)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-            ' USING _the_geom, _name, _product, _price, _contact, _address, _sched, _more;
+    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, name, supplies, contact, number, street_address, barangay, municipality_city, province, region, sched, more)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+            ' USING _the_geom, _name, _supplies, _contact, _number, _street_address, _barangay, _municipality_city, _province, _region, _sched, _more;
             
     RETURN 1;
 END;
@@ -33,4 +37,4 @@ $$
 LANGUAGE plpgsql SECURITY DEFINER ;
 
 --Grant access to the public user
-GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data(text, text, text, text, text, text, text, text) TO publicuser;
+GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data(text, text, text, text, text, text, text, text, text, text, text, text) TO publicuser;
